2957eb85aea6e1a45215699f31716e557b795e94
sanlouise <sandra.adamshallie@gmail.com>
2017-08-26 13:22:58 +0200

    Initial commit

---
+++ src/components/BaseLayout/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class BaseLayout extends Component {
+  render() {
+    return (
+      <div className="BaseLayout">
+        BaseLayout Page
+      </div>
+    );
+  }
+}
+
+export default BaseLayout;
---
+++ src/components/Contact/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Contact extends Component {
+  render() {
+    return (
+      <div className="Contact">
+        Contact Page
+      </div>
+    );
+  }
+}
+
+export default Contact;
