9a966a0a6a5f64f79b7adf24eec012963f75c9cd
sanlouise <sandra.adamshallie@gmail.com>
2017-08-26 13:42:00 +0200

    Add React Router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './globalStyles.css';
+import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom'
 import App from './components/App';
+import BaseLayout from './components/BaseLayout';
+import About from './components/About';
+import NavBar from './components/NavBar';
+import Contact from './components/Contact';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <BaseLayout>
+      <NavBar>
+        <Switch>
+          <Route path="/contact" component={Contact}/>
+          <Route path="/about" component={About}/>
+          <Route path="/" component={App}/>
+        </Switch>
+      </NavBar>
+    </BaseLayout>
+  </BrowserRouter>
+  ,
+  document.getElementById('root'));
 registerServiceWorker();
