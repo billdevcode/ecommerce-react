6325df90c96ea9d36bc87396a0704d68bd68e1b4
sanlouise <sandra.adamshallie@gmail.com>
2017-08-26 13:42:35 +0200

    Add about component

---
+++ src/components/About/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class About extends Component {
+  render() {
+    return (
+      <div className="container about text-center">
+        <h1>Welcome to the store</h1>
+      </div>
+    );
+  }
+}
+
+export default About;
