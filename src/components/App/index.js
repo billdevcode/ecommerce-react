86497ea920d902a4bf7af645f351a5e65c0822ce
sanlouise <sandra.adamshallie@gmail.com>
2017-08-29 22:36:09 +0200

    Import Header and ProductList

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
+import Header from '../Header/index.js';
+import ProductList from '../ProductList/index.js';
+
 import './index.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        App Page
-      </div>
-    );
+
+  constructor() {
+    super();
+
+    this.state = {
+    }
+
   }
+
+  render() {
+   return (
+      <div className="App">
+        <Header />
+        <div className="row">
+
+          <div className="col-sm-12">
+            <ProductList />
+          </div>
+        </div>
+      </div>
+   );
+ }
 }
 
 export default App;
