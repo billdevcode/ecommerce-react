4ccb43be3c08831a2d714abb09de5efd5771e1c3
sanlouise <sandra.adamshallie@gmail.com>
2017-08-29 22:37:31 +0200

    Add Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './index.css';
+
+const Header = () => (
+  <div className="row">
+    <div className="col header">
+      <h1 className="h1-header">Find Your New Favorite</h1>
+    </div>
+  </div>
+)
+
+export default Header;
