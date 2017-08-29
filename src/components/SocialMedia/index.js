f29303278a8a9260c3b40a3eb3dd3c877750f492
sanlouise <sandra.adamshallie@gmail.com>
2017-08-29 22:29:55 +0200

    Add social media components

---
+++ src/components/SocialMedia/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SocialMedia extends Component {
+  render () {
+    return (
+      <div className='social-media'>
+        <p>Follow us</p>
+      </div>
+    )
+  }
+}
+
+export default SocialMedia;
