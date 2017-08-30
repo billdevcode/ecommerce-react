04f68cbb2e5db32743853b2dbb1289a88ade1eb1
sanlouise <sandra.adamshallie@gmail.com>
2017-08-30 16:25:43 +0200

    Update footer styling

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,30 +6,42 @@ class Footer extends Component {
   render() {
     return (
       <footer className="page-footer black">
-          <div className="container">
-            <div className="row">
-              <div className="col l6 s12">
-                <h5 className="white-text">React E-Commerce App</h5>
-                <p className="grey-text text-lighten-4">This application was built with the best practices of React in mind. This project will be advanced through the implementation of Redux and the use of the API of Amazon.</p>
-              </div>
-              <div className="col l4 offset-l2 s12">
-                <h5 className="white-text">Sign Up</h5>
-                <ul>
-                  <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                  <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                  <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                  <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
-                </ul>
-              </div>
+        <div className="container">
+          <div className="row">
+
+            <div className="col l6 s12">
+              <h5 className="white-text">React E-Commerce App</h5>
+              <p className="grey-text text-lighten-4">This application was built with the best practices of React in mind. This project will be advanced through the implementation of Redux and the use of the API of Amazon.</p>
+            </div>
+
+            <div className="col l4 offset-l2 s12 center">
+              <h5>Stay Updated</h5>
+
+              <form action="#" method="post" className="validate" target="_blank" novalidate>
+
+                <div className="col s12 input-field">
+                  <label>Name</label>
+                  <input type="text" value="" />
+                </div>
+
+                <div className="col s12 input-field">
+                  <label>Email Address</label>
+                  <input type="email" value="" />
+                </div>
+
+                <div className="clear">
+                  <input type="submit" className="button Button" />
+                </div>
+              </form>
+
             </div>
           </div>
-          <div className="footer-copyright">
-            <div className="container">
-            © 2017 Copyright Sandra Adams-Hallie
-            <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
-            </div>
-          </div>
-        </footer>
+        </div>
+
+        <div className="footer-bottom center">
+          © 2017 Copyright Sandra Adams-Hallie
+        </div>
+      </footer>
     );
   }
 }
