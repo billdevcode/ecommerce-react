4e3e4a5bf5204e10a32951d4e59e1f4da74ab768
sanlouise <sandra.adamshallie@gmail.com>
2017-08-30 16:26:00 +0200

    Add ShowProduct functionality

--- src/components/ShowProduct/index.js
+++ src/components/ShowProduct/index.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
-import Product from './components/Product';
+import find from 'lodash/find';
 import PRODUCTS from '../../assets/data/products.js';
+import './index.css';
 
 class ShowProduct extends Component {
   render () {
-
-    let shownProduct = PRODUCTS.filter((product) => (
-      product.id === this.props.match.params)
-    )
+    const product = find(PRODUCTS, ['id', parseInt(this.props.match.params.id, 10)]);
 
     return (
-       <Product key={product.id} product={shownProduct} />
-    )
+      <div className="row center product-show">
+        <div className="col s12 m4 offset-m2">
+          <img className="product-image" src={product.img} alt="product" />
+        </div>
 
+        <div className="col s12 m6">
+          <div className="product-info">
+            <h3>{product.name}</h3>
+          </div>
+
+          <div className="product-bio">
+            <p>{product.description}</p>
+            <p>Size: {product.size}</p>
+            <p>Price: {product.price}</p>
+            <button type="button" className="button cart-button" >Add to cart</button>
+          </div>
+        </div>
+      </div>
+    );
   }
-}
-
-    return (
-      <div>Hello</div>
-    )
-  }
-
 }
 
 export default ShowProduct;
