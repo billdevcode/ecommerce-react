19bffcec3d943ed4e0f9bd7aaa6c997f8dece44b
sanlouise <sandra.adamshallie@gmail.com>
2017-09-16 19:27:18 -0400

    Add total amount to cart

--- src/containers/Cart/index.js
+++ src/containers/Cart/index.js
@@ -1,20 +1,48 @@
+// Dependencies
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import isEmpty from 'lodash/isEmpty';
+import map from 'lodash/map';
+
 
 class Cart extends Component {
+  computeTotalAmount = () => {
+    const { cartProducts } = this.props;
+    if (isEmpty(cartProducts)) {
+      return 0;
+    }
+
+    return cartProducts.reduce((sum, product) => {
+      sum += product.price;
+      return sum;
+    }, 0);
+  }
+
   render () {
-    console.log(this.props.cartProducts);
-    const products = this.props.cartProducts.map((product) => {
-      return (
-        <li key={product.id}>
-          {product.name}
-        </li>
-      )
-    })
     return (
       <div className='Cart'>
         <p>This is my cart</p>
-        { products }
+        <div className="cart-products">
+          {map(this.props.cartProducts, (product) => (
+            <div key={product.id}>
+              <div className="col s12 col m6 col l3">
+                <div className="cart-product-image">
+                  <img alt={product.name} src={product.img} />
+                </div>
+                <div className="cart-product-details">
+                  <h1 id="cart-product-name">{product.name}</h1>
+                  <h4 id="cart-product-description">{product.description}</h4>
+                </div>
+                <div className="price-add">
+                  <h5 id="cart-product-price">${product.price}</h5>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="cart-total-amount">
+          Total: {this.computeTotalAmount()}
+        </div>
       </div>
     )
   }
