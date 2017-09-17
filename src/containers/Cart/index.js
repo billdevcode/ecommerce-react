62f35f00a1de5cf5d5e045fedd4206eeae5db7af
sanlouise <sandra.adamshallie@gmail.com>
2017-09-17 18:25:13 -0400

    Update Cart Container styling

--- src/containers/Cart/index.js
+++ src/containers/Cart/index.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import isEmpty from 'lodash/isEmpty';
 import map from 'lodash/map';
+import styles from './styles.css';
 
 
 class Cart extends Component {
@@ -20,17 +21,17 @@ class Cart extends Component {
 
   render () {
     return (
-      <div className='Cart'>
-        <p>This is my cart</p>
-        <div className="cart-products">
+      <div className='Cart row'>
+        <div className="cart-products col s12 m8">
+        <h1 className="center">Your Cart</h1>
           {map(this.props.cartProducts, (product) => (
             <div key={product.id}>
-              <div className="col s12 col m6 col l3">
+              <div className="col s12 m6">
                 <div className="cart-product-image">
                   <img alt={product.name} src={product.img} />
                 </div>
                 <div className="cart-product-details">
-                  <h1 id="cart-product-name">{product.name}</h1>
+                  <h2 id="cart-product-name">{product.name}</h2>
                   <h4 id="cart-product-description">{product.description}</h4>
                 </div>
                 <div className="price-add">
@@ -40,8 +41,8 @@ class Cart extends Component {
             </div>
           ))}
         </div>
-        <div className="cart-total-amount">
-          Total: {this.computeTotalAmount()}
+        <div className="cart-sidebar col s12 m4">
+          Total: ${this.computeTotalAmount()}USD
         </div>
       </div>
     )
